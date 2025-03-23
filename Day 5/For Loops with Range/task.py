f9fc3951a24271a7fa817eedfbc980ef81d80402
patrickwright <pmwright@bu.edu>
2025-03-23 15:31:44 -0400

    Day 5 work

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,10 @@
+
+for number in range(1,11,3):
+    print(number)
+
+total_sum = 0
+
+for i in range(1,101):
+    total_sum += i
+
+print(total_sum)
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,31 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Easy version
+# my_password = ""
+#
+# for i in range(nr_letters):
+#     my_password += random.choice(letters)
+# for i in range(nr_symbols):
+#     my_password += random.choice(symbols)
+# for i in range(nr_numbers):
+#     my_password += random.choice(numbers)
+#
+# print(my_password)
 
+# Hard version
+password_lst = []
+
+for i in range(nr_letters):
+    password_lst.append(random.choice(letters))
+for i in range(nr_symbols):
+    password_lst.append(random.choice(symbols))
+for i in range(nr_numbers):
+    password_lst.append(random.choice(numbers))
+
+random.shuffle(password_lst)
+shuffled_password = ""
+for char in password_lst:
+    shuffled_password += char
+
+print(f"Your password is: {shuffled_password}")
