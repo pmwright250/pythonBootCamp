7fa74e2d999a040d992819859fff7f49db9d81cc
patrickwright <pmwright@bu.edu>
2025-04-14 00:02:12 -0400

    Day 8 progress

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,6 @@
+def greet(name):
+    print(f"Hello {name}!")
+    print("How are you?")
+    print("It is good to see you!")
+
+greet("Pat")
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -6,3 +6,10 @@ def greet_with_name(name):
 
 
 greet_with_name("Jack Bauer")
+
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Patrick","Ireland")
+greet_with(location="Spain",name="Jake")
