5cf44cba94cac8f7bc89d10491adc9cc91a45692
patrickwright <pmwright@bu.edu>
2025-07-28 00:27:30 -0400

    Day 10 calculator project updates

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,21 @@
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+}
+
+# Multiply function example
+print(operations["*"](4,8))
