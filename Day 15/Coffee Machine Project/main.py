f5c2f9104f1427868455ecdbbacfcf17b7c000fe
patrickwright <pmwright@bu.edu>
2025-08-20 22:22:51 -0400

    coffee machine project

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,3 +1,5 @@
+from importlib.resources import is_resource
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -30,7 +32,7 @@ resources = {
     "coffee": 100,
 }
 
-def calculate_change():
+def calculate_money():
     print("Please insert coins.")
     quarters = int(input("how many quarters?:"))
     dimes = int(input("how many dimes?:"))
@@ -38,23 +40,46 @@ def calculate_change():
     pennies = int(input("how many pennies?:"))
     return (quarters * 0.25) + (dimes * 0.10) + (nickels * 0.05) + (pennies * 0.01)
 
+def is_resource_sufficient(obj):
+    for item in resources:
+        if resources[item] < obj["ingredients"][item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
 def coffee_machine():
-    choice = input("What would you like? (espresso/latte/cappuccino):")
+    profit = 0
+    still_open = True
 
-    if choice == "off":
-        return
-    elif choice == "espresso":
-        obj = MENU["espresso"]
-        calculate_change()
-    elif choice == "latte":
-        obj = MENU["latte"]
-        calculate_change()
-    elif choice == "cappuccino":
-        obj = MENU["cappuccino"]
-        calculate_change()
-    elif choice == "report":
-        print(resources)
-    else:
-        return
+    while still_open:
+        choice = input("What would you like? (espresso/latte/cappuccino):")
 
-# print(calculate_change())
+        if choice == "off":
+            still_open = False
+        elif choice == "report":
+            print(f"Water: {resources['water']}ml")
+            print(f"Milk: {resources['milk']}ml")
+            print(f"Coffee: {resources['coffee']}g")
+            print(f"Money: ${profit}")
+        else:
+            if choice not in MENU:
+                print("Not a valid option.")
+                print("Try again!")
+            else:
+                obj = MENU[choice]
+                is_valid = is_resource_sufficient(obj)
+                if not is_valid:
+                    print("Sorry, maybe try another drink.")
+                else:
+                    money_amt = calculate_money()
+                    if money_amt - obj["cost"] < 0:
+                        print("Sorry that's not enough money. Money refunded.")
+                    else:
+                        profit += obj["cost"]
+                        change = round(money_amt - obj["cost"],2)
+                        for item in resources:
+                            resources[item] -= obj["ingredients"][item]
+                        print(f"Here is ${change} in change.")
+                        print(f"Here is your {choice}. Enjoy!")
+
+coffee_machine()
