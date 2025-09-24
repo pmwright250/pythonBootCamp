0740ee329272bfa2885e9a3d6c8ca597fcdd6c8b
patrickwright <pmwright@bu.edu>
2025-09-23 22:09:18 -0400

    Further day 16 notes

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,11 +1,21 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkGoldenrod1")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("DarkGoldenrod1")
-timmy.forward(100)
+from prettytable import PrettyTable
+table = PrettyTable()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table.add_column("Pokeman Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+
+print(table)
