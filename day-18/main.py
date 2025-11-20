17d223893a1634f368a9fcf7fd5841a31a54a91a
patrickwright <pmwright@bu.edu>
2025-11-20 08:51:42 -0500

    spirograph code

--- day-18/main.py
+++ day-18/main.py
@@ -1,12 +1,25 @@
+import random
+import turtle
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
+turtle.colormode(255)
 
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+timmy_the_turtle.speed("fastest")
+
+def draw_spirograph(size_of_gap):
+    for _ in range(360 // size_of_gap):
+        timmy_the_turtle.color(random_color())
+        timmy_the_turtle.circle(100)
+        timmy_the_turtle.setheading(timmy_the_turtle.heading() + size_of_gap)
+
+draw_spirograph(5)
 
 screen = Screen()
 screen.exitonclick()
