362e3978f7ffb393613a25ae74eab0c523d046e5
patrickwright <pmwright@bu.edu>
2025-12-29 18:26:05 -0500

    Completed turtle crossing game

--- turtle-crossing/scoreboard.py
+++ turtle-crossing/scoreboard.py
@@ -1,5 +1,25 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
+class Scoreboard(Turtle):
 
-class Scoreboard:
-    pass
+    def __init__(self):
+        super().__init__()
+        self.current_level = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-280,250)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.current_level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.current_level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align="center", font=FONT)
+
