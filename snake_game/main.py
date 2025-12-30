08b8d62d1ebb397cab67205d1b83992e58feafe5
patrickwright <pmwright@bu.edu>
2025-12-30 09:39:53 -0500

    snake game edits and opening/reading/writing to files

--- snake_game/main.py
+++ snake_game/main.py
@@ -30,16 +30,15 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.clear()
         scoreboard.increase_score()
 
     # Detect collision with wall
     if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
